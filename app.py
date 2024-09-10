8a35ff8fb985e98453c1adc037485856585b10ef
Taranga-grl <taranga@graniteriverlabs.in>
2024-09-10 17:37:20 +0530

    UI removed

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify
 import requests
 
 app = Flask(__name__)
-
-@app.route('/', methods=['GET'])
-def home():
-    return render_template('index.html')
 
 @app.route('/weather', methods=['GET'])
 def get_weather():
